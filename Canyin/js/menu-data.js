fbd06c42ca750f9a7e6688a920e9996272495e3c
Alan Chen <cmjethz@gmail.com>
2026-07-17 10:55:21 +0800

    Update menu-data.js

--- Canyin/js/menu-data.js
+++ Canyin/js/menu-data.js
@@ -16,7 +16,7 @@ const menuData = [
         name: "酸菜肥肠炒饭",
         category: "signature",
         price: 32,
-        description: "酸菜、卤肥肠、米饭、鸡蛋，配蒜末与葱花大火翻炒，咸香微酸",
+        description: "酸菜、卤肥肠、米饭、鸡蛋、葱花",
         image: "img/scfccf.jpg",
         badge: "signature",
         hot: true
@@ -26,7 +26,7 @@ const menuData = [
         name: "招牌六鲜炒饭",
         category: "signature",
         price: 32,
-        description: "甄选虾仁、鸡蛋、火腿、青豆、玉米与香菇，大火快炒，粒粒分明、鲜香浓郁",
+        description: "甄选虾仁、鸡蛋、火腿、青豆、玉米、香菇",
         image: "img/liuxianchfan.jpg",
         badge: "signature"
     },
@@ -37,7 +37,7 @@ const menuData = [
         name: "鸭血粉丝汤",
         category: "seafood",
         price: 22,
-        description: "鸭血，粉丝，鸭杂，油豆腐，香菜，葱花",
+        description: "鸭血、粉丝、鸭杂、香菜，葱花",
         image: "img/yxfst.jpg",
         badge: "hot"
     },
@@ -55,7 +55,7 @@ const menuData = [
         name: "冬瓜排骨汤",
         category: "seafood",
         price: 28,
-        description: "冬瓜，排骨，香油，葱，姜，花椒，食盐各适量",
+        description: "冬瓜，排骨，香油，葱，姜，花椒",
         image: "img/dgpgt.jpg",
         badge: "new"
         
@@ -65,7 +65,7 @@ const menuData = [
         name: "皮肚三鲜汤",
         category: "seafood",
         price: 20,
-        description: "皮肚，木耳，咸肉，菇，笋片，毛豆米",
+        description: "皮肚、木耳、咸肉、菇、笋片、毛豆米",
         image: "img/pidusxt.jpg"
     },
 
@@ -75,7 +75,7 @@ const menuData = [
         name: "酱爆鱿鱼炒面/河粉",
         category: "meat",
         price: 32,
-        description: "鱿鱼，青彩椒，红彩椒，葱，盐，油",
+        description: "鱿鱼、青彩椒、红彩椒、葱",
         image: "img/youyuchaom.jpg",
         hot: true
     },
@@ -100,7 +100,7 @@ const menuData = [
         name: "五彩虾仁炒面/河粉",
         category: "meat",
         price: 28,
-        description: "狮子头、面条、鸡蛋，香葱，创意融合",
+        description: "鲜嫩虾仁、搭配胡萝卜、玉米粒、青豆、彩椒",
         image: "img/wucxrcm.jpg",
         badge: "new"
     },
@@ -109,7 +109,7 @@ const menuData = [
         name: "香菇青菜炒面/河粉",
         category: "meat",
         price: 18,
-        description: "面条，鸡蛋，香菇，小青菜，香葱",
+        description: "面条、鸡蛋、香菇、小青菜、香葱",
         image: "img/xianggusumian.jpg"
     },
 
@@ -119,7 +119,7 @@ const menuData = [
         name: "雪碧可乐",
         category: "vegetable",
         price: 4,
-        description: "透心凉，渴释放",
+        description: "透心凉、渴释放",
         image: "img/cola.jpg"
     },
     {
@@ -127,7 +127,7 @@ const menuData = [
         name: "啤酒",
         category: "vegetable",
         price: 28,
-        description: "沁心啤酒，纯属生活",
+        description: "沁心啤酒、纯属生活",
         image: "img/beer_0.jpg",
         badge: "new"
     },
@@ -136,7 +136,7 @@ const menuData = [
         name: "毛铺苦荞酒",
         category: "vegetable",
         price: 26,
-        description: "荞韵流芳匠心酿，劲享品质毛铺香",
+        description: "荞韵流芳匠心酿、劲享品质毛铺香",
         image: "img/kuqiaojiu.jpg",
         hot: true
     },
